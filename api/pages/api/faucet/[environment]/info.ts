d4388fa6efcd87a7f46a208a96173570e806ede0
Jose Aguinaga <me@jjperezaguinaga.com>
2022-02-23 12:31:53 +0100

    feature(balance): See hoprnet/hoprnet#3351 - Faucet information via API

--- api/pages/api/faucet/[environment]/info.ts
+++ api/pages/api/faucet/[environment]/info.ts
@@ -1,10 +1,13 @@
-import { providers, Wallet, utils, errors } from 'ethers';
+import { providers, Wallet, utils, errors, Contract, BigNumber } from 'ethers';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { isValidEnvironment, protocolConfig, isValidNetwork } from '../../../../utils/protocol';
 
 type BalanceDataResponse = {
   address?: string
-  balance?: string
+  balance?: {
+    native: string,
+    hopr: string
+  }
   err?: string
 }
 
@@ -18,7 +21,8 @@ export default async function handler(
 
   try {
     const actualEnvironment = environment instanceof Array ? environment[0] : environment
-    
+    const abi = ["function balanceOf(address) view returns (uint)"];
+
     if (!isValidEnvironment(actualEnvironment)) return res.status(501).json({ err: `Environment is invalid, try any of the following: ${Object.keys(protocolConfig.environments)}`})
     const network = protocolConfig.environments[actualEnvironment].network_id;
     
@@ -27,10 +31,15 @@ export default async function handler(
 
     if (!process.env.FAUCET_SECRET_WALLET_PK) return res.status(501).json({ err: 'No faucet private key defined in server' })
     const wallet = new Wallet(process.env.FAUCET_SECRET_WALLET_PK, provider);
+    
+    const tokenAddressContract = protocolConfig.environments[actualEnvironment].token_contract_address
+    const hoprTokenContract = new Contract(tokenAddressContract, abi, provider);
     const address = await wallet.getAddress();
-    const balance = utils.formatEther(await wallet.getBalance());
 
-    res.status(200).json({ address, balance })
+    const native = utils.formatEther(await wallet.getBalance());
+    const hopr = utils.formatEther((await hoprTokenContract.balanceOf(address) as BigNumber));
+
+    res.status(200).json({ address, balance: { native, hopr } })
 
   } catch (err: any) {
     if (err.code == errors.INVALID_ARGUMENT) return res.status(422).json({ err: 'Address given is incorrect' })
